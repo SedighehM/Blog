ffb62267138775963b633cb16b22bb7d28fdc981
unknown <semohammadkhani@gmail.com>
2022-07-27 19:04:23 -0400

    blogs project

---
+++ src/reducers/index.js
@@ -0,0 +1,4 @@
+import { combineReducers } from "redux";
+import postsReducer from "./postsReducer";
+import usersReducer from "./usersReducer";
+export default combineReducers({ posts: postsReducer, users: usersReducer });
